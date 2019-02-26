113a7561b9faabe40b89c06a8ce64c18a84dc037
Andrew <andrewpeliza@hotmail.com>
2019-02-26 14:26:16 +0000

    Rendering Lists correctly using Flat List.

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-  const placesOutput = props.places.map((place, i) => (
-    <ListItem
-      key={i}
-      placeName={place}
-      onItemPressed={() => props.onItemDeleted(i)}
+  return (
+    <FlatList
+      style={styles.listContainer}
+      data={props.places}
+      renderItem={info => (
+        <ListItem
+          placeName={info.item.value}
+          onItemPressed={() => props.onItemDeleted(info.item.key)}
+        />
+      )}
     />
-  ));
-  return <View style={styles.listContainer}>{placesOutput}</View>;
+  );
 };
 
 const styles = StyleSheet.create({
