52f409c9fccaf33482cfcd7deeb36c6203df89f7
Andrew <andrewpeliza@hotmail.com>
2019-02-26 14:14:50 +0000

    Spread out content into its own components and delete item works.

--- App.js
+++ App.js
@@ -8,56 +8,42 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import ListItem from './src/components/ListItem/ListItem';
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 export default class App extends Component {
   state = {
-    placeName: '',
     places: []
   };
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(placeName)
+      };
     });
   };
 
-  placeSubmitHanlder = () => {
-    if (this.state.placeName.trim() === '') {
-      return;
-    }
-
+  placeDeletedHandler = index => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
       };
     });
   };
 
   render() {
-    const placesOutput = this.state.places.map((place, i) => (
-      // <Text key={i}>{place}</Text>
-      <ListItem key={i} placeName={place} />
-    ));
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            // style={{ width: 300 }}
-            placeholder='An awesome place!'
-            value={this.state.placeName}
-            onChangeText={this.placeNameChangedHandler}
-            style={styles.placeInput}
-          />
-          <Button
-            title='ADD'
-            style={styles.placeButton}
-            onPress={this.placeSubmitHanlder}
-          />
-        </View>
-        <View style={styles.listContainer}>{placesOutput}</View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
       </View>
     );
   }
@@ -66,25 +52,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
-    justifyContent: 'flex-start',
+    padding: 26,
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  inputContainer: {
-    // flex: 1,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  placeInput: {
-    width: '70%'
-  },
-  placeButton: {
-    width: '30%'
-  },
-  listContainer: {
-    width: '100%'
+    justifyContent: 'flex-start'
   }
 });
