a6ab54c8c3343cb32a09f7ac1926ef365519c89e
Andrew <andrewpeliza@hotmail.com>
2019-02-27 13:23:35 +0000

    Adding React Native Navigation

--- App.js
+++ App.js
@@ -1,85 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import { Navigation } from 'react-native-navigation';
 
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { connect } from 'react-redux';
+import AuthScreen from './src/screens/Auth/Auth';
 
-import * as actions from './src/store/actions/index';
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-import PlaceList from './src/components/PlaceList/PlaceList';
-// import placeImage from './src/assets/beautiful-place.jpg';
+// Register Screens
+Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
 
-class App extends Component {
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
-    console.log('Place Added', placeName);
-  };
-
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  };
-
-  modalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  };
-
-  placeSelectedHandler = key => {
-    this.props.onSelectPlace(key);
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail
-          selectedPlace={this.props.selectedPlace}
-          onItemDeleted={this.placeDeletedHandler}
-          onModalClosed={this.modalClosedHandler}
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList
-          places={this.props.places}
-          onItemSelected={this.placeSelectedHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 30,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start'
+// Start a App
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'awesome-places.AuthScreen',
+    title: 'Login'
   }
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: name => dispatch(actions.addPlace(name)),
-    onDeletePlace: () => dispatch(actions.deletePlace()),
-    onSelectPlace: key => dispatch(actions.selectPlace(key)),
-    onDeselectPlace: () => dispatch(actions.deselectPlace())
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
---
+++ index.android.js
@@ -0,0 +1,17 @@
+// import { AppRegistry } from 'react-native';
+// import React from 'react';
+// import { Provider } from 'react-redux';
+
+import App from './App';
+// import { name as appName } from './app.json';
+// import configureStore from './src/store/configureStore';
+
+// const store = configureStore();
+
+// const ReactNativeRedux = () => (
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
+
+// AppRegistry.registerComponent(appName, () => App);
