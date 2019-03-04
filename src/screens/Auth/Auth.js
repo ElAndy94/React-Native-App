8310a3aeda6fdfbf74aa13955e5c093dbe68bc48
Andrew <andrewpeliza@hotmail.com>
2019-03-04 11:56:52 +0000

    Changing the Auth Screen with the css requirements.

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 
@@ -10,12 +10,32 @@ class AuthScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Auth Screen</Text>
-        <Button title='Login' onPress={this.loginHandler} />
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
+        <Button title='Switch to Login' />
+        <View style={styles.inputContainer}>
+          <TextInput placeholder='Your Email' style={styles.input} />
+          <TextInput placeholder='Password' style={styles.input} />
+          <TextInput placeholder='Confirm Password' style={styles.input} />
+        </View>
+        <Button title='Submit' onPress={this.loginHandler} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    width: '100%'
+  }
+});
 
 export default AuthScreen;
