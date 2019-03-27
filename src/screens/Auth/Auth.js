4ce8454011d811d0cc50a50e7ce030c289c8580f
Andrew <andrewpeliza@hotmail.com>
2019-03-27 17:31:55 +0000

    Responsive solution #1 with dynamical styles depending on width/height.

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -9,6 +9,32 @@ import ButtonWithBackground from '../../components/UI/ButtonWithBackground/Butto
 import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
+  state = {
+    respStyles: {
+      pwContainerDirection: 'column',
+      pwContainerJustifyContent: 'flex-start',
+      pwWrapperWidth: '100%'
+    }
+  };
+
+  constructor(props) {
+    super(props);
+
+    Dimensions.addEventListener('change', dims => {
+      this.setState({
+        respStyles: {
+          pwContainerDirection:
+            Dimensions.get('window').height > 500 ? 'column' : 'row',
+          pwContainerJustifyContent:
+            Dimensions.get('window').height > 500
+              ? 'flex-start'
+              : 'space-between',
+          pwWrapperWidth: Dimensions.get('window').height > 500 ? '100%' : '45%'
+        }
+      });
+    });
+  }
+
   loginHandler = () => {
     startMainTabs();
   };
@@ -33,11 +59,16 @@ class AuthScreen extends Component {
           <View style={styles.inputContainer}>
             <DefaultInput placeholder='Your Email' style={styles.input} />
           </View>
-          <View style={styles.passwordContainer}>
-            <View style={styles.passwordWrapper}>
+          <View
+            style={{
+              flexDirection: this.state.respStyles.pwContainerDirection,
+              justifyContent: this.state.respStyles.pwContainerJustifyContent
+            }}
+          >
+            <View style={{ width: this.state.respStyles.pwWrapperWidth }}>
               <DefaultInput placeholder='Password' style={styles.input} />
             </View>
-            <View style={styles.passwordWrapper}>
+            <View style={{ width: this.state.respStyles.pwWrapperWidth }}>
               <DefaultInput
                 placeholder='Confirm Password'
                 style={styles.input}
